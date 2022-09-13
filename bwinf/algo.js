5cc94f7a717706fc0648e09a7888189abc06c4c2
haraldderbeistehende <101694456+haraldderbeistehende@users.noreply.github.com>
2022-09-13 20:56:04 +0200

    deleted patterns and build.js

--- bwinf/algo.js
+++ bwinf/algo.js
@@ -2,11 +2,5 @@ function mainAlgorithm(sudokus) {
     let progress = [];
     let input = sudokus[0];
     let output = sudokus[1];
-    let amountinput = getAmountOfDigits(input);
-    let amountoutput = getAmountOfDigits(output);
-    let sameNums = compareDigits(amountinput, amountoutput);
-    progress.push(input);
-    let out = changeDigits(sameNums, input);
-    numsChanged = out[0];
-    let change = out[1];
+    
 }
